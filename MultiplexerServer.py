52277df9c17da046c95e5696487702cbb4c1371a
buyukyilmaz <ugur.buyukyilmaz@gmail.com>
2016-11-08 17:40:45 +0300

    bitti sayilir

--- MultiplexerServer.py
+++ MultiplexerServer.py
@@ -1,12 +1,16 @@
 import socket
 from thread import *
+import time
 
 
 class MultiplexerServer:
     localhost = "127.0.0.1"
+    answerPage = "<!DOCTYPE html><html><body><H1>Thanks for answer</H1></body></html>"
+    timeoutPage = "<!DOCTYPE html><html><body><H1>Timeout</H1></body></html>"
 
     def __init__(self, multiplexerServerPort, quizServerPort):
         self.clients = {}
+        self.questionID = ""
         self.nextQuestion = False
         self.multiplexerServerPort = multiplexerServerPort
         self.quizServerPort = quizServerPort
@@ -19,16 +23,43 @@ class MultiplexerServer:
         self.serverSocketForClients.listen(10)
 
     def listenToClient(self, client, addr):
+
         while True:
-            clientMove = client.recv(2048)
-            if clientMove == '':
-                continue
-            elif clientMove[5] == 'Q':
-                self.sendQuestionToClient(client, clientMove)
-            elif clientMove[5] == 'S':
-                submittedQuestionId, selectedAnswer = self.parseClientAnswer(clientMove)
-                self.clients.setdefault(addr, []).append([submittedQuestionId, selectedAnswer])
-                print self.clients
+            try:
+                clientMove = client.recv(2048)
+            except socket.timeout:
+                client.send(MultiplexerServer.timeoutPage)
+                self.clients.setdefault(addr[0], []).append([self.questionID, "-1", "-1"])
+                break
+            self.answerTime = time.time()
+
+            if clientMove:
+                if clientMove[5] == 'Q':
+                    self.questionID = clientMove[14:clientMove.index(".")]
+                    self.sendQuestionToClient(client, clientMove)
+                elif clientMove[5] == 'S':
+                    submittedQuestionId, selectedAnswer = self.parseClientAnswer(clientMove)
+
+                    if self.clients.get(addr[0]):
+                        for i in self.clients[addr[0]]:
+                            if i[0] == submittedQuestionId:
+                                client.close()
+                                return
+
+                    self.resultTime = self.answerTime - self.askTime
+                    self.clients.setdefault(addr[0], []).append([submittedQuestionId, selectedAnswer, self.resultTime])
+                    client.send('HTTP/1.1 200 OK\n')
+                    client.send('Content-Type: text/html\n')
+                    client.send('\n')
+                    client.send(MultiplexerServer.answerPage)
+                    print self.clients
+                    break
+                else:
+                    break
+            else:
+                break
+
+        client.close()
 
     def parseClientWish(self, clientWish):
         result = clientWish.split("\n")
@@ -53,10 +84,33 @@ class MultiplexerServer:
         connectedClient.send('Content-Type: text/html\n')
         connectedClient.send('\n')
         connectedClient.send(question)
+        self.askTime = time.time()
 
 if __name__ == "__main__":
     aa = MultiplexerServer(13000, 12000)
 
     while True:
         connectedClient, addr = aa.serverSocketForClients.accept()
+        connectedClient.settimeout(30)
         start_new_thread(aa.listenToClient, (connectedClient, addr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
